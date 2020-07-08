35c6118cb4ddd011cf33a38f11e79b4422721187
Geovana Menezes <56615628+geovanamenezes@users.noreply.github.com>
2020-07-08 15:07:41 -0300

    Update calculoIdade.php

--- calculoIdade.php
+++ calculoIdade.php
@@ -44,7 +44,7 @@
 		</h3>
 		<a href="menu.php"> Ir ao menu de exercícios </a><br>
 		<a href="ex6.php"> Próximo exercício </a><br>
-		<a href="ex4.php"> Voltar ao exercício anterior </a>
+		<a href="aprovadoOuReprovado.php"> Voltar ao exercício anterior </a>
 
 	 </form>
 
