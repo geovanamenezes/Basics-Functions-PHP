9831f3158419d8da76e5afdbfac09fa8c1c6daa8
Geovana Menezes <56615628+geovanamenezes@users.noreply.github.com>
2020-07-08 15:21:21 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="text-align: center;">
+
+	<h1> Lista de exercícios WEB </h1>
+	<h3> Geovana Menezes Amorim </h3>
+	<a href="ex1.php">Exercício 1</a>
+	<br>
+	<a href="ex2.php">Exercício 2</a>
+	<br>
+	<a href="ex3.php">Exercício 3</a>
+	<br>
+	<a href="ex4.php">Exercício 4</a>
+	<br>
+	<a href="ex5.php">Exercício 5</a>
+	<br>
+	<a href="ex6.php">Exercício 6</a>
+	<br>
+	<a href="ex7.php">Exercício 7</a>
+	<br>
+	<a href="ex8.php">Exercício 8</a>
+	<br>
+	<a href="ex9.php">Exercício 9</a>
+	<br>
+	<a href="ex10.php">Exercício 10</a>
+	<br>
+	<a href="ex11.php">Exercício 11</a>
+	<br>
+	<a href="ex12.php">Exercício 12</a>
+
+
+</body>
+</html>
