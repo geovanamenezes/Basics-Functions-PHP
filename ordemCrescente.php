62ec78b22cf616af56270af4d698498ed387cba9
Geovana Menezes <56615628+geovanamenezes@users.noreply.github.com>
2020-07-16 13:50:25 -0300

    Update ordemCrescente.php

--- ordemCrescente.php
+++ ordemCrescente.php
@@ -52,7 +52,7 @@
 		 		
 		</h3>
 		<a href="menu.php"> Ir ao menu de exercícios </a><br>
-		<a href="ex8.php"> Próximo exercício </a><br>
+		<a href="calculoDuracao.php"> Próximo exercício </a><br>
 		<a href="qualOMaior.php"> Voltar ao exercício anterior </a>
 
 	 </form>
